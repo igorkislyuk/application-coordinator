7543f56daa3249b96d347fbb250cdb843d2b075b
Igor Kislyuk <glyerkg@gmail.com>
2016-11-26 16:58:04 +0300

    Initial commit.

---
+++ ApplicationCoordinator/AppDelegate.m
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.m
+//  ApplicationCoordinator
+//
+//  Created by Igor on 26/11/16.
+//  Copyright © 2016 Igor Kislyuk. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+//@property (nonatomic, strong) 
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    // Override point for customization after application launch.
+    return YES;
+}
+
+
+@end
---
+++ ApplicationCoordinator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ApplicationCoordinator
+//
+//  Created by Igor on 26/11/16.
+//  Copyright © 2016 Igor Kislyuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
